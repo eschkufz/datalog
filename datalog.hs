f17ae647e5c20835494bc7692d53ddae3a056d63
eric <eschkufz@kokako.(none)>
2011-05-06 20:09:35 -0700

    Substitution application. Show for Sub.

--- datalog.hs
+++ datalog.hs
@@ -37,8 +37,30 @@ instance Show Term where
     showTerms [] = ""
     showTerms (x:xs) = " " ++ show x ++ showTerms xs
 
+-- Artificial Intelligence a Modern Approach (3rd edition):
+-- TODO: Copy the definition of substitutions
+
+data Sub = Sub (Map.Map Term Term)
+
+instance Show Sub where
+  show (Sub m) = "{" ++ showSubs (Map.toList m) ++ " }" where
+    showSubs [] = ""
+    showSubs ((v,t):ss) = " " ++ show v ++ "/" ++ show t ++ showSubs ss
+
+-- Artificial Intelligence a Modern Approach (3rd edition):
+-- (Text), page 323
+
+subst :: Term -> Sub -> Term
+subst (Constant c) _ = (Constant c)
+subst (Variable v) (Sub m) = case Map.lookup (Variable v) m of
+  (Just t) -> subst t (Sub m)
+  Nothing -> (Variable v)
+subst (Function p xs) theta = (Function p (substTerms xs theta)) where
+  substTerms [] _ = []
+  substTerms (x:xs) theta = subst x theta : substTerms xs theta
+
 -- Artificial Intelligence a Modern Approach (3rd edition): 
--- Figure 9.1, page 398
+-- Figure 9.1, page 328
 --
 -- function UNIFY(x, y, theta) returns a substitution to make x and y identical
 -- inputs: x, a variable, constant, list, or compound
@@ -67,10 +89,8 @@ instance Show Term where
 --
 -- * Prolog omits this
 
-type Sub = Map.Map Term Term
-
 unify :: Term -> Term -> Maybe Sub
-unify x y = unify' x y (Just Map.empty)
+unify x y = unify' x y (Just (Sub Map.empty))
 
 unify' :: Term -> Term -> Maybe Sub -> Maybe Sub
 unify' _ _ Nothing = Nothing
@@ -79,14 +99,31 @@ unify' x y theta
 unify' (Variable x) y theta = unifyVar (Variable x) y theta
 unify' x (Variable y) theta = unifyVar (Variable y) x theta
 unify' (Function p xs) (Function q ys) theta
-  | (p /= q) || (length p /= length q) = Nothing
+  | (p /= q) || (length xs /= length ys) = Nothing
   | otherwise = foldl (\t (x,y) -> unify' x y t) theta (zip xs ys)
 unify' _ _ _ = Nothing
 
 unifyVar :: Term -> Term -> Maybe Sub -> Maybe Sub
-unifyVar var x (Just theta)
-  | Map.member var theta = case Map.lookup var theta of
-    (Just val) -> unify' val x (Just theta)
-  | Map.member x theta = case Map.lookup x theta of
-    (Just val) -> unify' x val (Just theta)
-  | otherwise = (Just (Map.insert var x theta))
+unifyVar _ _ Nothing = Nothing
+unifyVar var x (Just (Sub m))
+  | Map.member var m = case Map.lookup var m of
+    (Just val) -> unify' val x (Just (Sub m))
+  | Map.member x m = case Map.lookup x m of
+    (Just val) -> unify' x val (Just (Sub m))
+  | otherwise = Just (Sub (Map.insert var x m))
+
+-- Artificial Intelligence a Modern Approach (3rd edition):
+-- Figure 9.3, page 332 
+-- Function FOL-BC-ASK(KB, goals, theta) returns a set of substitutions
+-- inputs: KB, a knowledge base
+--         goals, a list of conjuncts forming a query (theta already applied) 
+--         theta, the current substitution, initially the empty substitution {}
+-- local variables: answers, a set of substitutions, initially empty
+--
+-- if goals is empty then return {theta}
+-- qDelta <- SUBST(theta, FIRST(goals))
+-- for each sentence r in KB where STANDARDIZE-APART(r) = (p1 ^ ... ^ pn => q)
+--     and thetaDelta <- UNIFY(q, qDelta) succeeds
+--   new_goals <- [p1,...,pn|REST(goals)]
+--   answers <- FOL-BC-ASK(KB, new_goals, COMPOSE(thetaDelta, theta)) U answers
+-- return answers
